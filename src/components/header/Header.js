13b7067eb1184bf0477a9593e6c05ae2e2d41264
Yasin Elbüz <yasinelbuz@gmail.com>
2023-03-27 04:02:34 +0300

    styles:add header links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from './header.module.scss';
 import Search from "@/components/search/Search";
+import Link from "next/link";
 
 export default function Header() {
    return <>
       <header className={styles.header}>
+         <div>
+            <div className="container">
+               <nav className={styles.navigation}>
+                  <Link href="/repos">Repos</Link>
+                  <Link href="/repos">Follower</Link>
+                  <Link href="/repos">Following</Link>
+                  <Link href="/repos">Non-Followers</Link>
+               </nav>
+
+            </div>
+         </div>
          <Search />
       </header>
    </>
