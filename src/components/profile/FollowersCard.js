fa1de371d9c40f1972449bb2e9e1b8a04d54aea3
Yasin Elbüz <yasinelbuz@gmail.com>
2023-03-27 07:40:22 +0300

    fix:follower button problem

--- src/components/profile/FollowersCard.js
+++ src/components/profile/FollowersCard.js
@@ -22,7 +22,7 @@ const FollowersCard = () => {
         <div className={`${styles.card} ${styles['followers-card']}`}>
             {!isLoading && <div className={styles.followers}>
                 {followers?.map(followers => <Follower key={followers.id} followers={followers} />)}
-                {(data.length != 0 && followers.length == 50) && <button onClick={loadFollowers}>
+                {(data.length != 0 && data.length == 50) && <button onClick={loadFollowers}>
                     {isFetching ? "Fetching..." : "Go on"}
                 </button>}
             </div>}
