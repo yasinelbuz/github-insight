23ac057f50579c9c1adc9de84d7f9a2c4162cee2
Yasin Elbüz <yasinelbuz@gmail.com>
2023-03-29 10:13:34 +0300

    refactor:moved head component index to _app file

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import StatsCard from '@/components/card/stats/StatsCard';
 import Profile from '@/components/card/info/Profile';
@@ -7,26 +6,16 @@ const NextFusionCharts = dynamic(
     () => import('@/components/fusioncharts/FusionChart'),
     { ssr: false }
 );
-import { useGetGithubUserByNameQuery, useGetGithubUserByFollowersQuery, useGetGithubUserByReposQuery } from '@/services/users';
+import { useGetGithubUserByNameQuery, useGetGithubUserByReposQuery } from '@/services/users';
 import { useSelector } from 'react-redux';
 
 export default function HomeContainer() {
     const { search } = useSelector((state) => state.users);
-    const { data, isError } = useGetGithubUserByNameQuery(search);
+    const { data } = useGetGithubUserByNameQuery(search);
     const { data: reposData, isLoading } = useGetGithubUserByReposQuery(search);
 
     return (
         <>
-            <Head>
-                <title>Github Info</title>
-                <meta name="description" content="Find github user info" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
             {!isLoading && <main>
                 <StatsCard data={data} />
                 <Profile data={data} />
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,25 @@
-import '@/styles/globals.scss'
-import { store } from '@/redux/store'
-import { Provider } from 'react-redux'
 import Header from '@/layout/header/Header'
+import Head from 'next/head';
+import { Provider } from 'react-redux'
+import { store } from '@/redux/store'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Header />
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <title>Github Info</title>
+        <meta name="description" content="Find github user info" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider store={store}>
+        <Header />
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
