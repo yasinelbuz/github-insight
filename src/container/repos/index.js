8abe13d330238351edf9dd6c22db158d736f7c6f
Yasin Elbüz <yasinelbuz@gmail.com>
2023-03-29 10:18:01 +0300

    refactor:updated repos page

--- src/container/repos/index.js
+++ src/container/repos/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Table from "@/components/table/table";
 import { useGetGithubUserByReposQuery } from "@/services/users";
 import { useSelector } from "react-redux";
@@ -8,35 +8,39 @@ export default function ReposContainer() {
    const { search } = useSelector(state => state.users);
    const { data: repos, isLoading } = useGetGithubUserByReposQuery(search);
 
+   const tableHead = [
+      { name: 'Repo name', sortable: true, width: "200px" },
+      { name: 'Description' },
+      { name: 'Star count', sortable: true },
+      { name: 'Forks count', sortable: true },
+      { name: 'Size', sortable: true },
+      { name: 'Is Fork?', sortable: true },
+      { name: 'Created at', sortable: true, width: "125px" },
+      { name: 'Repo url' },
+   ];
+
+   const tableBody = repos.map(repo => ([
+      <a href={repo.html_url} style={{ color: "#000", textDecoration: "underline" }} target="_blank">{repo.name}</a>,
+      repo.description,
+      repo.stargazers_count,
+      repo.forks_count,
+      repo.size,
+      repo.fork ? "Yes" : "No",
+      repo.created_at.slice(0, 10),
+      <a href={repo.html_url} style={{ color: "#000", textDecoration: "underline" }} target="_blank">Repo</a>,
+   ]))
+
    return <>
       <main>
          <section>
             <div className="container">
                {!isLoading ? <Table
                   searchable={true}
-                  head={[
-                     { name: 'Repo name', sortable: true, width: "200px" },
-                     { name: 'Description' },
-                     { name: 'Star count', sortable: true },
-                     { name: 'Forks count', sortable: true },
-                     { name: 'Size', sortable: true },
-                     { name: 'Is Fork?', sortable: true },
-                     { name: 'Created at', sortable: true, width: "125px" },
-                     { name: 'Repo url' },
-                  ]}
-                  body={repos && repos.map((repo, key) => ([
-                     <a href={repo.html_url} style={{ color: "#000", textDecoration: "underline" }} target="_blank">{repo.name}</a>,
-                     repo.description,
-                     repo.stargazers_count,
-                     repo.forks_count,
-                     repo.size,
-                     repo.fork ? "Yes" : "No",
-                     repo.created_at.slice(0, 10),
-                     <a href={repo.html_url} style={{ color: "#000", textDecoration: "underline" }} target="_blank">Repo</a>,
-                  ]))}
-               /> : "LoadiRg"}
+                  head={tableHead}
+                  body={repos && tableBody}
+               /> : "Loading"}
             </div>
          </section>
-      </main >
+      </main>
    </>
 }
