553259576fb5c8cbdce98ba97cd0c2fe5cb48b98
Yasin Elbüz <yasinelbuz@gmail.com>
2023-04-10 01:16:02 +0300

    refactor:Translated a Turkish sentence into English

--- src/components/follow/noUserMessage.js
+++ src/components/follow/noUserMessage.js
@@ -2,5 +2,5 @@ import React from "react";
 import styles from './style.module.scss';
 
 export default function NoUserMessage() {
-   return <div className={styles.no_user__message}>Gösterilecek başka kullanıcı kalmadı...</div>;
+   return <div className={styles.no_user__message}>There are no more users to display....</div>;
 }
