5644ba4d44ebb26b0353f2df92b3ecea8435d9a6
Yasin Elbüz <yasinelbuz@gmail.com>
2023-04-09 03:12:32 +0300

    style:added new icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,4 +1,4 @@
-export const Repos = () => {
+export const Repos = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -9,6 +9,7 @@ export const Repos = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path
                 fillRule="evenodd"
@@ -17,7 +18,8 @@ export const Repos = () => {
         </svg>
     );
 };
-export const Followers = () => {
+
+export const Followers = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -30,6 +32,7 @@ export const Followers = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
             <circle cx="9" cy="7" r="4"></circle>
@@ -39,7 +42,7 @@ export const Followers = () => {
     );
 };
 
-export const Following = () => {
+export const Following = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -52,6 +55,7 @@ export const Following = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
             <circle cx="8.5" cy="7" r="4"></circle>
@@ -60,7 +64,7 @@ export const Following = () => {
         </svg>
     );
 };
-export const Gists = () => {
+export const Gists = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -71,6 +75,7 @@ export const Gists = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path
                 fillRule="evenodd"
@@ -79,7 +84,7 @@ export const Gists = () => {
         </svg>
     );
 };
-export const Location = () => {
+export const Location = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -89,13 +94,14 @@ export const Location = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path fill="none" d="M0 0h24v24H0z"></path>
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 010-5 2.5 2.5 0 010 5z"></path>
         </svg>
     );
 };
-export const Company = () => {
+export const Company = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -105,13 +111,14 @@ export const Company = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path fill="none" d="M0 0h24v24H0z"></path>
             <path d="M12 7V3H2v18h20V7H12zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10zm-2-8h-2v2h2v-2zm0 4h-2v2h2v-2z"></path>
         </svg>
     );
 };
-export const LinkIcon = () => {
+export const LinkIcon = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -121,13 +128,14 @@ export const LinkIcon = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path fill="none" d="M0 0h24v24H0z"></path>
             <path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path>
         </svg>
     );
 };
-export const SearchIcon = () => {
+export const SearchIcon = (props) => {
     return (
         <svg
             stroke="currentColor"
@@ -137,6 +145,7 @@ export const SearchIcon = () => {
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
         >
             <path fill="none" d="M0 0h24v24H0z"></path>
             <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
@@ -149,6 +158,7 @@ export const StarIcon = (props) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
+            {...props}
             fill="none"
             viewBox="0 0 24 24"
             width="16px"
@@ -163,11 +173,54 @@ export const StarIcon = (props) => {
 
 export const HamburgerIcon = (props) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <svg xmlns="http://www.w3.org/2000/svg"
+            {...props}
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
     );
 };
+
+
+export const NonFollowersIcon = (props) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            {...props}
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384" />
+        </svg>
+
+    );
+};
+
+export const RateIcon = (props) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            {...props}
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+        </svg>
+    );
+};
+
+
+
+
+
+
+
 
 
 
